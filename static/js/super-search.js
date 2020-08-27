05b3086f4f79951537c55d1b1e59671e22d950f2
Kristofer <kriscarta@gmail.com>
2020-08-27 08:22:56 +0200

    Fix broken styling of dates in search results

--- static/js/super-search.js
+++ static/js/super-search.js
@@ -130,7 +130,7 @@
             searchResultsEl.classList.remove('is-hidden');
             searchResultsEl.innerHTML = matchingPosts.map(function (post) {
                 d = new Date(post.pubDate);
-                return '<li><a class="search-result" href="' + post.link + '">' + post.title + '<span class="search__result-date">' + d.toUTCString().replace(/.*(\d{2})\s+(\w{3})\s+(\d{4}).*/,'$2 $1, $3') + '</span></a></li>';
+                return '<li><a class="search-result" href="' + post.link + '">' + post.title + '<div class="search__result-date">' + d.toUTCString().replace(/.*(\d{2})\s+(\w{3})\s+(\d{4}).*/,'$2 $1, $3') + '</div></a></li>';
             }).join('');
         }
         lastSearchResultHash = currentResultHash;
